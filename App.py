61bb5bb20748b3515c2e5b1e0fc3d241661e7cd1
sowndaryasree <sowndaryasree2007@gmail.com>
2025-09-17 11:47:47 +0530

    update

--- App.py
+++ App.py
@@ -1,13 +1,13 @@
 from flask import Flask
-App=Flask(__name__)
+app = Flask(__name__)   # lowercase
 
-@App.route('/')
+@app.route('/')
 def home():
     return "Home"
 
-@App.route('/about' )
+@app.route('/about')
 def about():
     return "about1"
 
-if __name__=="__main__":
-    App.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
