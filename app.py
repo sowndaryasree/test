03379ec9fb850d7781de3eba11282339014700a2
sowndaryasree <sowndaryasree2007@gmail.com>
2025-09-17 11:52:45 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)   # lowercase
+
+@app.route('/')
+def home():
+    return "Home"
+
+@app.route('/about')
+def about():
+    return "about1"
+
+if __name__ == "__main__":
+    app.run(debug=True)
